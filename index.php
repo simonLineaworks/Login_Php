d745f796730a8879abf119d6f314cc81837fe0dc
simonLineaworks <simon@lineaworks.jp>
2017-07-20 18:18:06 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once('authenticate.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>LOGIN PHP</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+<body>
+<div id="page">
+	<h1>YOU ARE LOGIN</h1>
+	<a href='logout.php'>Click here to log out</a>
+</div>
+<!-- [/page] -->
+</body>
+</html>
